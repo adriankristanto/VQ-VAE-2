9cb1689d9924a3d855d7cbe66cb9c6f21626202c
Adrian Kristanto <49110670+adriankristanto@users.noreply.github.com>
2020-08-31 22:48:40 +1000

    added the implementation of the top decoder

--- code/models/Decoders.py
+++ code/models/Decoders.py
@@ -48,3 +48,41 @@ class BottomDecoder(nn.Module):
     def forward(self, x):
         x = self.layers(x)
         return x
+
+class TopDecoder(nn.Module):
+
+    """
+    The top decoder will upsample the input by a factor of 2.
+    For example, given an input of shape (32, 32, hidden_channels) => (64, 64, hidden_channels)
+    """
+
+    def __init__(self, in_channels, hidden_channels, num_resblocks, res_channels, out_channels):
+        super(TopDecoder, self).__init__()
+        self.layers = self._build(in_channels, hidden_channels, num_resblocks, res_channels, out_channels)
+    
+    def _build(self, in_channels, hidden_channels, num_resblocks, res_channels, out_channels):
+        layers = [
+            # here, we want to use the same padding to keep the output size the same as the input size
+            # padding = (s(n - 1) - n + f) / 2
+            # ((n - 1) -n + 3) / 2 = 1
+            nn.Conv2d(in_channels=in_channels, out_channels=hidden_channels, kernel_size=3, stride=1, padding=1)
+        ]
+        layers += [
+            # here, we create num_resblocks number of residual blocks
+            ResidualBlock(hidden_channels, res_channels, hidden_channels) for _ in range(num_resblocks)
+        ]
+        layers += [
+            # each resblock output is not wrapped by the activation function as the next block has ReLU as its first layer
+            # however, the final resblock doesn't have anything to wrap its output with an activation function
+            # therefore, here we wrap the output of the final resblock with ReLU
+            nn.ReLU()
+        ]
+        layers += [
+            # p = (2(in - 1) + 4 - 2in) / 2 = 1
+            nn.ConvTranspose2d(in_channels=hidden_channels, out_channels=out_channels, kernel_size=4, stride=2, padding=1)
+        ]
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        x = self.layers(x)
+        return x
