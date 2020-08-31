317b0403a10183e7df9366df39c59e1a0ade32e3
Adrian Kristanto <49110670+adriankristanto@users.noreply.github.com>
2020-08-31 21:02:56 +1000

    added references

---
+++ code/models/Encoders.py
@@ -0,0 +1,7 @@
+import torch
+import torch.nn as nn
+from Residual import ResidualBlock
+
+# references: 
+# https://github.com/rosinality/vq-vae-2-pytorch/blob/master/vqvae.py
+# https://github.com/unixpickle/vq-vae-2
