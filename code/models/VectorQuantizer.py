added additional fix for the backprop problem

code/models/VectorQuantizer.py
@@ -129,6 +129,10 @@ class VectorQuantizerEMA(nn.Module):
         # NOTE: the EMA formula is 
         # gamma * previous_term + (1 - gamma) * the current term
         if self.training:
+
+            encodings_sum = torch.sum(encodings, dim=0)
+            embedding_sum = torch.matmul(x_flatten.transpose(0, 1), encodings)
+
             # the first one is N, or the cluster size
             # NOTE: in the paper, n_i^(t) is the number of input vectors that are quantized into a specific 
             # embedding vector
@@ -137,7 +141,7 @@ class VectorQuantizerEMA(nn.Module):
             # self.cluster_size = self.cluster_size * self.gamma + (1 - self.gamma) * torch.sum(encodings, dim=0)
             # inplace methods to save memory
             self.cluster_size.mul_(self.gamma).add_(
-                torch.sum(encodings, dim=0), alpha=1 - self.gamma
+                encodings_sum, alpha=1 - self.gamma
             )
             
             # print(torch.sum(encodings, dim=0).shape) # torch.Size([512])
@@ -157,7 +161,7 @@ class VectorQuantizerEMA(nn.Module):
             # self.embed_avg = self.embed_avg * self.gamma + (1 - self.gamma) * torch.matmul(x_flatten.transpose(0, 1), encodings)
             # inplace operation to save memory
             self.embed_avg.mul_(self.gamma).add_(
-                torch.matmul(x_flatten.transpose(0, 1), encodings), alpha=1 - self.gamma
+                embedding_sum, alpha=1 - self.gamma
             )
 
             # according to https://github.com/zalandoresearch/pytorch-vq-vae/blob/master/vq-vae.ipynb
@@ -169,7 +173,7 @@ class VectorQuantizerEMA(nn.Module):
             )
             
             # final update: the weights of the embedding layer
-            embedding_normalised = self.embed_avg / cluster_size
+            embedding_normalised = self.embed_avg / cluster_size.unsqueeze(0)
             self.embedding.copy_(embedding_normalised)
             # print(self.embed_avg.shape) # torch.Size([64, 512])
             # print(cluster_size.shape) # torch.Size([512])