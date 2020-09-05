6f7e85cdda9dd3554de26773c84a6b528e0e2d60
Adrian Kristanto <49110670+adriankristanto@users.noreply.github.com>
2020-09-05 16:28:23 +1000

    set inplace=True for relu activation function in BottomDecoder class

--- code/models/Decoders.py
+++ code/models/Decoders.py
@@ -34,12 +34,12 @@ class BottomDecoder(nn.Module):
             # each resblock output is not wrapped by the activation function as the next block has ReLU as its first layer
             # however, the final resblock doesn't have anything to wrap its output with an activation function
             # therefore, here we wrap the output of the final resblock with ReLU
-            nn.ReLU()
+            nn.ReLU(inplace=True)
         ]
         layers += [
             # p = (2(in - 1) + 4 - 2in) / 2 = 1
             nn.ConvTranspose2d(in_channels=hidden_channels, out_channels=hidden_channels // 2, kernel_size=4, stride=2, padding=1),
-            nn.ReLU(),
+            nn.ReLU(inplace=True),
             # p = (2(in - 1) + 4 - 2in) / 2 = 1
             nn.ConvTranspose2d(in_channels=hidden_channels//2, out_channels=out_channels, kernel_size=4, stride=2, padding=1)
         ]
